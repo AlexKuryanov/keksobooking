24984ed47e0ff1f05ceb26357a486e2303608262
AlexKuryanov <lexxus07@yandex.ru>
2022-11-10 08:07:52 +0100

    feat: add func to create offers

--- js/main.js
+++ js/main.js
@@ -78,3 +78,36 @@ const Location = {
   Y: +getRandomNum(LongitudeInterval.MIN, LongitudeInterval.MAX, 5),
 }
 
+const offers = [];
+const createOffer = () => {
+  for (let i = 1; i <= OFFERS_COUNT; i++) {
+    offers.push({
+      author: {
+        avatar: `img/avatars/user${i < 10 ? '0' + i : i}.png`,
+      },
+      offer: {
+        title: `${getRandomArrayElement(TITLES)}`,
+        address: `${Location.X}, ${Location.Y}`,
+        price: +getRandomNum(Prices.MIN, Prices.MAX, 0),
+        type: `${getRandomArrayElement(OFFER_TYPES)}`,
+        rooms: +getRandomNum(RoomsCount.MIN, RoomsCount.MAX, 0),
+        guests: +getRandomNum(GuestsCount.MIN, GuestsCount.MAX, 0),
+        checkin: `${getRandomArrayElement(CHECKIN_TIMES)}`,
+        checkout: `${getRandomArrayElement(CHECKOUT_TIMES)}`,
+        features: getRandomArrayLength(FEATURES),
+        description: `${getRandomArrayElement(DESCRIPTION)}`,
+        photos: getRandomArrayLength(PHOTOS),
+      },
+      location: {
+        x: Location.X,
+        y: Location.Y,
+      }
+    })
+  }
+
+  return offers;
+}
+
+createOffer();
+
+console.log(offers);
